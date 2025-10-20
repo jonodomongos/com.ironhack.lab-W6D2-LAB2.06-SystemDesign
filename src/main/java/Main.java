0cb75e13b90c4e9436f9e1bbce080cae862a365c
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 12:13:43 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+	1. Crea un nuevo proyecto Java en tu entorno de desarrollo preferido.
+	2. En el proyecto, crea una clase llamada Estudiante que contenga las siguientes propiedades:
+	    - nombre: tipo String
+        - calificacion: tipo int (en el rango 0–100)
+	3.	Crea un Map con una clave del tipo String (el nombre del estudiante) y un valor del tipo Estudiante.
+	4.	Crea un método llamado incrementarCalificaciones que reciba como parámetro un Map<String, Estudiante>, aumente la nota de cada estudiante un 10 % y devuelva el mapa actualizado.
+	5.	Crea 4 objetos Estudiante (por ejemplo, representando a tus compañeros de clase) y añádelos al Map.
+	6.	Crea un diagrama de casos de uso y un diagrama de clases para un sistema de banca en línea.
+	    - El sistema debe tener las siguientes características:
+	    - Los usuarios pueden transferir fondos, retirar dinero de sus cuentas y depositar dinero.
+	    - Los administradores pueden congelar cuentas en caso de fraude.
+	    - En el diagrama de clases, utiliza la clase Dinero.
+     */
+    }
+}
